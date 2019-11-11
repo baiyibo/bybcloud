189254e0e9eb8ccaf2c0bb80f1b948b1e147ec1a
byb <236182321@qq.com>
2019-11-11 17:42:23 +0800

    cloud-config

---
+++ ribbon/src/main/java/com/cloud/ribbon/ConfigInfo.java
@@ -0,0 +1,22 @@
+package com.cloud.ribbon;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName ConfigInfo
+ * @Description TODO
+ * @Author BaiYiBo
+ * @Date 2019/11/11 16:05
+ * @Version 1.0.0
+ **/
+@Component
+@Data
+@ConfigurationProperties(prefix = "data")
+public class ConfigInfo {
+
+    private String env;
+    private String version;
+
+}
